25ca63e13309f84123fe04e0d22538068dee0db2
dunght160387 <dunght1603@gmail.com>
2015-06-05 13:51:18 +0700

    multi-views for phone-list and phone-detail. Added filter but filter not running :(

---
+++ app/phoneapp/filter/filter.js
@@ -0,0 +1,13 @@
+/**
+ * Created by dunght163 on 6/4/15.
+ */
+(function () {
+    //'use strict';
+    //
+    //angular.module('phoneappFilter', [])
+    //    .filter('checkmark', filterCheckmark);
+    //
+    //function filterCheckmark(input) {
+    //    return input ? '*YES*' : '*NO*'; //or use: (\u2713 -> ✓) or false (\u2718 -> ✘)
+    //}
+})();
